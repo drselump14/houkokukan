68e07240c36e3cbfc7e742e39ead36a87b9339f4
drselump14 <botoksgonzales@gmail.com>
2015-11-01 12:10:34 +0900

    enable json api for plain route

--- app/models/node.rb
+++ app/models/node.rb
@@ -13,4 +13,7 @@
 #
 
 class Node < ActiveRecord::Base
+  def self.convert_from_node_id_to_coordinate(node_ids)
+    where(node_id: node_ids).select(:latitude, :longitude).map { |node| [node.latitude, node.longitude] }
+  end
 end
